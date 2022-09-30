a2d8a0bb2aca0872bcb34a8d32795823a60a63a3
viniciustdaik <96190816+viniciustdaik@users.noreply.github.com>
2022-09-30 11:48:10 -0300

    Delete e.js

--- e.js
+++
@@ -1,5 +0,0 @@
-A América Central 
-
- 
-
-A América Central tem 20 países e uma série de ilhas que são parte de outros território (francês, britânico, estadunidense ou norte-americano, holandês) e é uma das áreas mais pobres da América. Nela vivem mais de 181 milhões de pessoas, está localizada no Hemisfério Norte entre o oceano Pacífico e o mar do Caribe. Já que está localizada em uma área de encontro de placas tectônicas por isso é possível sofrer fenômenos, como terremotos e erupções vulcânicas, e também ocorrências climáticas como tempestades tropicais e furacões. 
